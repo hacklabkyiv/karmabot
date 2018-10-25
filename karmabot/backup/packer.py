54c0eba329557ae6e1793932da70131fd3ded967
Evgen Druzhynin <evgen.druzhynin@gmail.com>
2018-10-25 16:30:19 +0300

    Fix a vulnerability in pycrypto==2.6.1 by moving to `cryptography` module.

--- karmabot/backup/packer.py
+++ karmabot/backup/packer.py
@@ -2,12 +2,13 @@ import os
 import random
 import struct
 import lzma
-from Crypto.Cipher import AES
+import base64
+from cryptography.fernet import Fernet
 
 
 class Packer:
     """
-    packer/unpacker with LZMA compression and AES [CBC mode] encryption
+    packer/unpacker with LZMA compression and AES in CBC mode with a 128-bit key encryption
     """
 
     __slots__ = ['_key', '_chunk_size']
@@ -20,7 +21,7 @@ class Packer:
         self._key = ['0'] * 32
         for c in range(min(32, len(key))):
             self._key[c] = key[c]
-        self._key = ''.join(self._key)
+        self._key = base64.urlsafe_b64encode(''.join(self._key).encode('utf-8'))
 
         self._chunk_size = chunksize
 
@@ -30,16 +31,9 @@ class Packer:
         """
         with open(filename, 'rb') as f:
             with open(output_filename, 'wb') as e:
-                iv = bytes(random.randint(0, 0xFF) for i in range(16))
-                encryptor = AES.new(self._key, AES.MODE_CBC, iv)
+                encryptor = Fernet(self._key)
 
-                e.write(iv)
-                # reserve the space for a size record
-                e.write(struct.pack('<Q', 0))
-
-                compressed_file_size = 0
                 buffer = bytes()
-
                 data = f.read(self._chunk_size)
                 while data:
                     buffer += lzma.compress(data)
@@ -47,33 +41,18 @@ class Packer:
                         compressed_data = buffer[:self._chunk_size]
                         buffer = buffer[self._chunk_size:]
 
-                        compressed_file_size += self._chunk_size
                         e.write(encryptor.encrypt(compressed_data))
                     data = f.read(self._chunk_size)
 
-                oversize = len(buffer) % 16
                 if buffer:
-                    # don't include trailing zeros
-                    compressed_file_size += len(buffer)
-
-                    if oversize:
-                        buffer += b'0' * (16 - oversize)
                     e.write(encryptor.encrypt(buffer))
-
-                if compressed_file_size:
-                    e.seek(16, 0)
-                    e.write(struct.pack('<Q', compressed_file_size))
-                    e.seek(0, 2)
 
     def unpack(self, filename, output_filename):
         with open(filename, 'rb') as e:
             with open(output_filename, 'wb') as f:
-                iv = e.read(16)
-                decryptor = AES.new(self._key, AES.MODE_CBC, iv)
+                decryptor = Fernet(self._key)
 
-                compressed_file_size = struct.unpack('<Q', e.read(struct.calcsize('Q')))[0]
                 buffer = bytes()
-
                 data = e.read(self._chunk_size)
                 while data:
                     buffer += decryptor.decrypt(data)
@@ -81,12 +60,8 @@ class Packer:
                         decrypted_data = buffer[:self._chunk_size]
                         buffer = buffer[self._chunk_size:]
 
-                        compressed_file_size -= self._chunk_size
                         f.write(lzma.decompress(decrypted_data))
-                    
                     data = e.read(self._chunk_size)
 
                 if buffer:
-                    assert len(buffer) <= self._chunk_size
-                    buffer = buffer[:compressed_file_size]
                     f.write(lzma.decompress(buffer))
